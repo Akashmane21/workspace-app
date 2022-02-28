a6f58fb11b6af038f001f0cf09bb878aa12fe109
Akashmame <79107396+Akashmane21@users.noreply.github.com>
2022-02-28 18:55:06 +0530

    nav addeddfd

--- Comps/Nav.js
+++ Comps/Nav.js
@@ -56,6 +56,56 @@ const MaterialUISwitch = styled(Switch)(({ theme }) => ({
   },
 }));
 
+const IOSSwitch = styled((props) => (
+  <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
+))(({ theme }) => ({
+  width: 42,
+  height: 26,
+  padding: 0,
+  '& .MuiSwitch-switchBase': {
+    padding: 0,
+    margin: 2,
+    transitionDuration: '300ms',
+    '&.Mui-checked': {
+      transform: 'translateX(16px)',
+      color: '#fff',
+      '& + .MuiSwitch-track': {
+        backgroundColor: theme.palette.mode === 'dark' ? '#2ECA45' : '#65C466',
+        opacity: 1,
+        border: 0,
+      },
+      '&.Mui-disabled + .MuiSwitch-track': {
+        opacity: 0.5,
+      },
+    },
+    '&.Mui-focusVisible .MuiSwitch-thumb': {
+      color: '#33cf4d',
+      border: '6px solid #fff',
+    },
+    '&.Mui-disabled .MuiSwitch-thumb': {
+      color:
+        theme.palette.mode === 'light'
+          ? theme.palette.grey[100]
+          : theme.palette.grey[600],
+    },
+    '&.Mui-disabled + .MuiSwitch-track': {
+      opacity: theme.palette.mode === 'light' ? 0.7 : 0.3,
+    },
+  },
+  '& .MuiSwitch-thumb': {
+    boxSizing: 'border-box',
+    width: 22,
+    height: 22,
+  },
+  '& .MuiSwitch-track': {
+    borderRadius: 26 / 2,
+    backgroundColor: theme.palette.mode === 'light' ? '#E9E9EA' : '#39393D',
+    opacity: 1,
+    transition: theme.transitions.create(['background-color'], {
+      duration: 500,
+    }),
+  },
+}));
 
 export default function Nav() {
   const { isTheme,setisTheme} = useCounter();
@@ -75,13 +125,20 @@ export default function Nav() {
       <h1 style={{color:isTheme ? "white" : "black" , paddingTop:"10px"}}>LinkData</h1>
       </div>
       <div>
-      <Chip style={{color:isTheme ? "white" : "black" , position:"relative" , left:"30px"}} label="Akash Mane" avatar={<Avatar src="https://firebasestorage.googleapis.com/v0/b/reactcrud-7b0fc.appspot.com/o/Image%2Fy.png?alt=media&token=bd47121d-8a24-4b00-99dd-ed8e36ced485" />} />
-      <FormControlLabel
+      <Chip style={{color:isTheme ? "white" : "black" , position:"relative" , left:"10px"}} label="Akash Mane" avatar={<Avatar src="https://firebasestorage.googleapis.com/v0/b/reactcrud-7b0fc.appspot.com/o/Image%2Fy.png?alt=media&token=bd47121d-8a24-4b00-99dd-ed8e36ced485" />} />
+      {/* <FormControlLabel
         onChange={ChangeTheme}
         style={{position:"relative" , left:"30px"}}
         control={<MaterialUISwitch sx={{ m: 1 }}  />}
         label=""
         
+      /> */}
+       <FormControlLabel
+               onChange={ChangeTheme}
+               style={{position:"relative" , left:"30px"}}
+
+        control={<IOSSwitch sx={{ m: 1 }} defaultChecked />}
+        label=""
       />
       </div>
     </div>
